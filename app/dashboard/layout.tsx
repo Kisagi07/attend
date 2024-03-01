42d4802547137516f6bf0d92aca576f8f8af0c5d
Kisagi <shinkazumi7@gmail.com>
2024-03-01 10:12:21 +0700

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -11,9 +11,16 @@ const Layout: React.FC<Layout> = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
   return (
     <>
-      <nav className={`p-2  border-b h-14  border-slate-200 bg-white text-slate-600 shadow flex items-center`}>
+      <nav
+        className={`p-2  border-b h-14  border-slate-200 bg-white text-slate-600 shadow flex items-center`}
+      >
         <h1 className="text-xl font-sixtyfour uppercase italic font-bold">Attendance</h1>
-        <button onClick={() => setSidebarOpen(!sidebarOpen)} type="button" className="ml-auto hover:bg-slate-100 p-2 lg:hidden" title="open sidebar">
+        <button
+          onClick={() => setSidebarOpen(!sidebarOpen)}
+          type="button"
+          className="ml-auto hover:bg-slate-100 p-2 lg:hidden"
+          title="open sidebar"
+        >
           <FaBars className="text-xl " />
         </button>
       </nav>
@@ -22,8 +29,8 @@ const Layout: React.FC<Layout> = ({ children }) => {
         <Sidebar.LinkItem href="/dashboard" name="Dashboard" />
         <Sidebar.LinkItem href="/dashboard/employees" subMatch name="Employee" />
         <Sidebar.LinkItem href="/dashboard/job-positions" subMatch name="Job Position" />
-        <Sidebar.LinkItem href="/dashboard/salary" name="Salary" />
-        <Sidebar.LinkItem href="/dashboard/settings" name="Settings" />
+        {/* <Sidebar.LinkItem href="/dashboard/salary" name="Salary" />
+        <Sidebar.LinkItem href="/dashboard/settings" name="Settings" /> */}
         <Sidebar.Divider />
         <Logout className="!bg-white !text-inherit hover:!bg-slate-100 w-full p-2   text-left" />
       </Sidebar>
