119af93cc015ee9c4fe2c86b0f647c4ab784173e
Kisagi <shinkazumi7@gmail.com>
2024-05-30 14:22:00 +0700

    Add NextUI for new component library (still use flowbite for unchanged old pages)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,19 @@
+import { nextui } from "@nextui-org/theme";
 import type { Config } from "tailwindcss";
 import flowbite from "flowbite-react/tailwind";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/(button|input|link|ripple|spinner).js",
+    "./app/**/*.{js,jsx,ts,tsx}",
     flowbite.content(),
   ],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       animation: {
         "fade-up": "fade-up 150ms forwards",
@@ -23,6 +24,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [flowbite.plugin()],
+  plugins: [flowbite.plugin(), nextui()],
 };
 export default config;
