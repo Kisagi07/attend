b43e73dd7897f79c6beb771d811751a568c75794
Kisagi <shinkazumi7@gmail.com>
2024-09-02 10:43:05 +0700

    prisma client method for calculation total overtime of logs

--- app/prisma.ts
+++ app/prisma.ts
@@ -1,4 +1,5 @@
-import { Prisma, PrismaClient, ProjectHistory, users } from "@prisma/client";
+import { Prisma, PrismaClient, ProjectHistory, users, logs as Log } from "@prisma/client";
+import { parseTime } from "@internationalized/date";
 
 const prisma = new PrismaClient({
   omit: {
@@ -31,6 +32,62 @@ const prisma = new PrismaClient({
             file: history.file ? `${process.env.APP_URL}/api/public${history.file}` : null,
           } as any;
         },
+      },
+    },
+  })
+  .$extends({
+    name: "After hour overtime",
+    client: {
+      totalOvertime: (
+        logs: Log[],
+        { unit }: { unit: "hour" | "minutes" } = { unit: "minutes" }
+      ) => {
+        // initialize total minutes
+        let totalMinutes = 0;
+        // loop through logs
+        logs.forEach((log) => {
+          // check if log are overtime day also clock out time are not null
+          if (log.isOverTime && log.clock_out_time) {
+            // parse clock in and out time
+            const clockInTime = parseTime(
+              (log.clock_in_time as unknown as string).split("T")[1].split(".")[0]
+            );
+            const clockOutTime = parseTime(
+              (log.clock_out_time as unknown as string).split("T")[1].split(".")[0]
+            );
+            // calculate compare result
+            const compareResult = clockOutTime.compare(clockInTime);
+            // turn into minute
+            const resultInMinute = compareResult / 1000 / 60;
+            // add to total minutes
+            totalMinutes += resultInMinute;
+            return;
+          }
+          // check if log are after hour overtime
+          if (log.afterHourOvertime) {
+            // parse clock in and out time
+            const clockInTime = parseTime(
+              (log.clock_in_time as unknown as string).split("T")[1].split(".")[0]
+            );
+            const clockOutTime = parseTime(
+              (log.clock_out_time as unknown as string).split("T")[1].split(".")[0]
+            );
+            // calculate compare result
+            const compareResult = clockOutTime.compare(clockInTime);
+            // turn into minute
+            const resultInMinute = compareResult / 1000 / 60;
+            // add to total minutes
+            totalMinutes += resultInMinute;
+            return;
+          }
+        });
+
+        // determine return in hour or minute
+        if (unit === "hour") {
+          return totalMinutes / 60;
+        } else {
+          return totalMinutes;
+        }
       },
     },
   });
