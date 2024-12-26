9665d5ee0a07cb9d67229c54b3d9f37e671a30ed
Kisagi07 <shinkazumi7@gmail.com>
2024-12-26 15:17:38 +0700

    Implement logic to fetch the best employee based on attendance logs and job position, excluding admin roles

--- app/api/attendances/best/route.ts
+++ app/api/attendances/best/route.ts
@@ -1,21 +1,78 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/prisma";
+import { fromDate, parseDate, parseTime } from "@internationalized/date";
 
 const GET = async (req: NextRequest) => {
-  // fetch users with `logs` and `job_position`
+  // get today's date
+  const now = fromDate(new Date(), "asia/jakarta");
+  const nextYear = now.add({ years: 1 });
+  const prevYear = now.add({ years: -1 });
+  // fetch users with `logs` and `job_position` between last year and next year
   const users = await prisma.users.findMany({
     include: {
       logs: {
         where: {
-            
-        }
+          date: {
+            lte: nextYear.toDate(),
+            gte: prevYear.toDate(),
+          },
+        },
       },
       job_position: true,
     },
+    where: {
+      role: {
+        not: "admin",
+      },
+    },
   });
+  // get company tolerance time
+  const company = await prisma.company.findFirst();
+  const tolerance = company?.tolerance_time ?? 0;
+  // score to determine the best employee
+  let score: Record<string, number> = {};
+  let devi = null;
+  // iterate over users
+  users.forEach((user) => {
+    if (user.job_position) {
+      // calculate all logs tha have type of `work_from_home`, `work_from_office`, and `special_attendance`
+      const logs = user.logs.filter(
+        (log) =>
+          log.type === "work_from_home" ||
+          log.type === "work_from_office" ||
+          log.type === "special_attendance"
+      );
+      // initial scrone
+      let initialScore = logs.length;
+      // get user job position shift_start time
+      const shiftStart = parseTime(user.job_position.shift_start);
+      // add company tolerance time to shiftStart
+      const shiftStartTolerance = shiftStart.add({ minutes: tolerance });
+      // iterate logs
+      logs.forEach((log) => {
+        // get log clock_in_time as string with format `HH:mm:ss` from date string
+        const utcClockInTime = log.clock_in_time;
+        const clockInTime = parseTime(
+          `${utcClockInTime?.getUTCHours().toString().padStart(2, "0")}:${utcClockInTime?.getUTCMinutes().toString().padStart(2, "0")}:${utcClockInTime?.getUTCSeconds().toString().padStart(2, "0")}`
+        );
+        // check if clockInTime is late
+        const isLate = clockInTime.compare(shiftStartTolerance) > 0;
+        // if isLate is true then decrease initialScore
+        if (isLate) {
+          initialScore -= 1;
+        }
+      });
+      // set user score
+      score[user.name!] = initialScore;
+    }
+  });
+  // get the highest score user
+  const bestEmployee = Object.keys(score).reduce((a, b) =>
+    score[a] > score[b] ? a : b
+  );
 
   // return
-  return NextResponse.json(users);
+  return NextResponse.json(bestEmployee);
 };
 
 export { GET };
