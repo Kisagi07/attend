a52b4560ce9c842c763f314996b4cabcb8f90e67
Kisagi <119020432+Kisagi07@users.noreply.github.com>
2025-02-17 12:47:13 +0700

    Update seed.ts

--- app/seed.ts
+++ app/seed.ts
@@ -12,7 +12,17 @@ async function main() {
     },
   });
 
-  const admin = await prisma.users.create({
+  const softwareDeveloper = await prisma.job_positions.create({
+    data: {
+      name: "Software Developer",
+      salary: 0,
+      shift_start: "09:00",
+      shift_end: "16:00"
+    }
+  })
+
+  // Create admin 
+   await prisma.users.create({
     data: {
       name: "Admin",
       password: await bcryptjs.hash("290990", 10),
@@ -26,6 +36,21 @@ async function main() {
       },
     },
   });
+  // create normal employee
+  await prisma.users.create({
+    data: {
+      name: "Zufri",
+      password: await bcryptjs.hash("777777",10),
+      role: "employee",
+      gender: "male",
+      "work_id": "ID002",
+      job_position: {
+        connect: {
+          id: softwareDeveloper.id
+        }
+      }
+    }
+  })
 }
 
 main()
