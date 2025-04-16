eb719105fe806b4c55c9fc2803c192e172020116
Kisagi <119020432+Kisagi07@users.noreply.github.com>
2025-04-16 14:14:16 +0700

    fix: wrong import package

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -2,7 +2,7 @@ import { formatRupiah } from "@/app/helper";
 import prisma, { HistoryWithUser, ProjectResult } from "@/app/prisma";
 import { authorized } from "@/app/serverhelper";
 import { $Enums } from "@/prisma/client";
-import { PrismaClientKnownRequestError } from "@/prisma/client/runtime/library";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "../../auth/[...nextauth]/authConfig";
 import "@/initExtension";
