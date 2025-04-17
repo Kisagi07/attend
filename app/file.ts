672638aa470724678184448240fd34129405561d
Kisagi <119020432+Kisagi07@users.noreply.github.com>
2025-04-17 12:25:18 +0700

    feat: compressed image if file size exceed the set maxSizeMB

--- app/file.ts
+++ app/file.ts
@@ -2,10 +2,11 @@ import { existsSync, mkdirSync } from "fs";
 import { writeFile, unlink } from "fs/promises";
 import { join } from "path";
 import { v4 as uuidv4 } from "uuid";
+import sharp from "sharp"
 
 export async function storeFile(
   file: File,
-  { uniqueName, storePath = "" }: { uniqueName?: boolean; storePath?: string },
+  { uniqueName, storePath = "", maxSizeMB = 1 }: { uniqueName?: boolean; storePath?: string, maxSizeMB?: number },
 ) {
   const filename = uniqueName
     ? uuidv4() + "." + file.name.split(".").pop()
@@ -19,8 +20,24 @@ export async function storeFile(
     mkdirSync(dir, { recursive: true });
   }
 
+  const maxSizeBytes = maxSizeMB * 1024 * 1024;
+
+  let finalBuffer = buffer;
+
+  if (buffer.length > maxSizeBytes) {
+    let quality = 80;
+    let compressedBuffer = buffer;
+
+    do {
+      compressedBuffer = await sharp(buffer).toFormat("jpeg", { quality }).toBuffer();
+      quality -= 10;
+    } while (compressedBuffer.length > maxSizeBytes && quality > 10);
+
+    finalBuffer = compressedBuffer;
+  }
+
   const path = join(process.cwd(), `public${storePath}`, filename);
-  await writeFile(path, buffer);
+  await writeFile(path, finalBuffer);
   return storePath + "/" + filename;
 }
 
